5f9ee739599c2fcb9dd9ff62a5eb171e58b62075
reniar <reniar@reniar.com>
2020-03-21 23:00:01 +0100

    Revert "Revert "Correct UserTest localhost to offline.test domain""
    This reverts commit b443eebfebe6f6fcf4ff11bedb40bff892f2afd1.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 
     <div class="mr-auto"><p class="text-4xl">The collective for a global vision</p></div>
-
+    <div class="mr-auto"><p class="text-4xl">The collective for a global vision</p></div>
 
 @endsection
