bd11c46a6b8c2c530a2e938c37dd955ad6e03deb
reniar <reniar@reniar.com>
2020-04-22 17:29:15 +0200

    Update nav.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,8 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd(request()->server());
-
         $channels = Channel::all();
 
         return view('home', compact('channels'));
