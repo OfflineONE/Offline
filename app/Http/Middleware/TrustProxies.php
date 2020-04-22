47d27d71d06b8c58c686b0d4f94b0ce063510994
reniar <reniar@reniar.com>
2020-04-22 17:27:07 +0200

    Revert "TEST DELETE"
    This reverts commit 4e459a8ba091a5c06c621c087d4859f10e4e352f.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,12 +7,13 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    protected $proxies = '*';
     /**
      * The trusted proxies for this application.
      *
      * @var array|string
      */
+    protected $proxies;
+
     /**
      * The headers that should be used to detect proxies.
      *
