439e415c5c07b8345dc41bfa832500ff2ad1d2e5
Reniar <reniar@reniar.com>
2020-05-05 16:07:56 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/StartController.php
+++ app/Http/Controllers/StartController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class StartController extends Controller
 {
-
     public function index()
     {
         return view('start');
     }
-    
-
 }
