99e41529d9066b6da90b24171d31d9eac8a9692b
reniar <reniar@reniar.com>
2020-05-07 21:52:29 +0200

    Revert "test"
    This reverts commit dd2b05dd0d74083b3cba4220b996d9c403793746.

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -9,23 +9,23 @@
          factory('App\User')->create([
              'id' => '1',
              'name' => 'nobody',
-             'email' => 'test@test.test',
+             'email' => env('ADMIN_1'),
              'password' => bcrypt(env('PW_1')),
          ]);
 
-//         factory('App\User')->create([
-//             'id' => '2',
-//             'name' => 'reniar',
-//             'email' => env('ADMIN_2'),
-//             'password' => bcrypt(env('PW_1')),
-//         ]);
-//
-//         factory('App\User')->create([
-//             'id' => '3',
-//             'name' => 'nitai',
-//             'email' => env('ADMIN_3'),
-//             'password' => bcrypt(env('PW_1')),
-//         ]);
+         factory('App\User')->create([
+             'id' => '2',
+             'name' => 'reniar',
+             'email' => env('ADMIN_2'),
+             'password' => bcrypt(env('PW_1')),
+         ]);
+
+         factory('App\User')->create([
+             'id' => '3',
+             'name' => 'nitai',
+             'email' => env('ADMIN_3'),
+             'password' => bcrypt(env('PW_1')),
+         ]);
 
 //         factory('App\User', 50)->create();
      }
